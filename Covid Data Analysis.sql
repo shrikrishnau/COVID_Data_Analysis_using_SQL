9b35d94213687898b6f35616bd2cc1081aded5e8
Shri Krishna Uthirasamy <116000153+shrikrishnau@users.noreply.github.com>
2022-11-17 20:46:52 +0530

    Update Covid Data Analysis.sql

--- Covid Data Analysis.sql
+++ Covid Data Analysis.sql
@@ -10,9 +10,6 @@ from PortfolioProject..CovidDeaths
 Where continent is not null 
 order by 3,4
 
---select * 
---from PortfolioProject..CovidVaccinations
---order by 3,4
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject..CovidDeaths
